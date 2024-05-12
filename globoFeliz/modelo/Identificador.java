0b5c09115459922185c54c6c05c2a2afd9ab2dbd
Fernando7Cordero <145577399+Fernando7Cordero@users.noreply.github.com>
2024-05-11 19:34:52 -0600

    Add files via upload

---
+++ globoFeliz/modelo/Identificador.java
@@ -0,0 +1,73 @@
+/* 
+Clase: Identificador
+(Esta clase sirve para generar un id aleatorio de 3 digitos) 
+
+Autor: Fernando Cordero
+Version: 1.0
+*/
+
+package globoFeliz.modelo;
+
+import globoFeliz.modelo.*;
+import java.util.Random;
+
+public class Identificador {
+
+
+// ATRIBUTOS
+
+    Random random = new Random();
+
+    String tipoId;
+
+    int numId;
+
+    String id;
+
+    
+// CONSTRUCTOR
+
+    public Identificador (String tipo) {
+
+        setTipoId(tipo); 
+        setNumId();
+        setId();
+    }
+
+
+// SETTERS
+
+    private void setTipoId(String tipo) {
+
+        this.tipoId = tipo;
+    }
+
+    private void setNumId() {
+
+        this.numId = random.nextInt(900) + 100;
+    }
+
+    private void setId() {
+
+        this.id = getTipoId() + "-" + getNumId();
+    }
+
+// GETTERS
+
+    public String getTipoId () {
+
+        return tipoId;
+    }
+
+    public int getNumId () {
+
+        return numId;
+    }
+
+    public String getId () {
+
+        return id;
+    }
+
+   
+}
